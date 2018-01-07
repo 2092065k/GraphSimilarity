e3a288806e8399e8358f7449dc905e3066f3f5c6
Ivan Kyosev <Ivan Kyosev>
2018-01-07 18:41:21 +0200

    API outline for graph generator

---
+++ graphSimilarity/graph_generator.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+
+def generate_graphs_file(name, num_of_graphs, num_of_nodes, seed,
+                         weighted = False, weight_range = [1, 10],
+                         dense_regions = [], sparse_regions = []):
+    
+    np.random.seed(seed)
+    
+    return 0
