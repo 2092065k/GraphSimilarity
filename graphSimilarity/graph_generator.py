a25d6598c96b14c3c9e8c16fe5f03687053ac4b4
Ivan Kyosev <Ivan Kyosev>
2018-01-11 20:52:46 +0000

    The graph generator now supports dense and sparse regions

--- graphSimilarity/graph_generator.py
+++ graphSimilarity/graph_generator.py
@@ -1,20 +1,77 @@
 import numpy as np
-from math import floor
 
 
-def __get_num_of_edges(num_of_nodes):
-    'The number of edges will be equal to 30% - 60% of the edges in a clique'
+def __get_num_of_edges(num_of_nodes, clique_range):
+    'The number of edges will be equal to a percentage of the edges in a clique'
 
     edges_in_clique = (num_of_nodes * (num_of_nodes - 1)) / 2
-    percentage = (np.random.randint(30, high = 60) + np.random.random()) / 100.0
-    num_of_edges = int(floor(edges_in_clique * percentage))
+    percentage = (np.random.randint(clique_range[0], clique_range[1]) + np.random.random()) / 100.0
+    num_of_edges = int(round(edges_in_clique * percentage))
 
     return num_of_edges
 
 
+def __get_edge_weight(weighted, weight_range):
+
+    if not weighted:
+        weight = 1
+    else:
+        weight = np.random.randint(weight_range[0], clique_range[1] + 1)
+
+    return weight
+
+
+def __generate_edges(num_of_nodes, clique_range, weighted, weight_range, directed, dense_regions, sparse_regions):
+
+    #add the weights at the end
+    num_of_edges =  __get_num_of_edges(num_of_nodes, clique_range)
+    edges = []
+
+    for e in range(num_of_edges):
+
+        approved = False
+
+        while not approved:
+
+            # generate an edge in one of the dense regions
+            if len(dense_regions) > 0 and np.random.random() < 0.40:
+                region = dense_regions[np.random.randint(0, len(dense_regions))]
+                node1 = np.random.randint(region[0], region[1])
+                node2 = np.random.randint(region[0], region[1])
+                edge = [node1, node2]
+
+            # generate an edge in the rest of the graphs
+            else:
+                node1 = np.random.randint(0, num_of_nodes)
+                node2 = np.random.randint(0, num_of_nodes)
+                edge = [node1, node2]
+
+                # if the edge is from one of the sparse regions possibly reject it
+                for region in sparse_regions:
+                    if edge[0] in range(region[0], region[1]) and edge[1] in range(region[0], region[1]):
+                        if np.random.random() < 0.85:
+                            edge = [0, 0]
+                            break
+
+            if not directed:
+                edge.sort()
+
+            # currently excluding self edges
+            if edge[0] != edge[1] and edge not in edges:
+                edges.append(edge)
+                approved = True
+
+    # include a weight for each edge
+    for edge in edges:
+        weight = __get_edge_weight(weighted, weight_range)
+        edge.append(weight)
+
+    return edges
+
+
 def generate_graphs_file(file_name, num_of_graphs, num_of_nodes, seed,
-                         weighted = False, weight_range = [1, 10],
-                         dense_regions = [], sparse_regions = []):
+                         clique_range = [30, 60], weighted = False, weight_range = [1, 10],
+                         directed = False, dense_regions = [], sparse_regions = []):
     
     np.random.seed(seed)
     file = open(file_name, 'w')
@@ -23,16 +80,10 @@ def generate_graphs_file(file_name, num_of_graphs, num_of_nodes, seed,
 
         file.write(str(num_of_nodes) + '\n')
 
-        num_of_edges =  __get_num_of_edges(num_of_nodes)
+        edges = __generate_edges(num_of_nodes, clique_range, weighted, weight_range,
+                                directed, dense_regions, sparse_regions)
 
-        for edge in range(num_of_edges):
-
-            # somehow retry if edge already exists
-            node1 = np.random.randint(0, high = num_of_nodes)
-            node2 = np.random.randint(0, high = num_of_nodes)
-            weight = 1
-
-            file.write(str(node1) + ' ' + str(node2) + ' ' + str(weight) + '\n')
+        for edge in edges:
+            file.write(str(edge[0]) + ' ' + str(edge[1]) + ' ' + str(edge[2]) + '\n')
 
     file.close()
-    return 0
