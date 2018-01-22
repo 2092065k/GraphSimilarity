9b4843f3c8599549feca7d5e5ab0356571db691c
2092065k <2092065k@student.gla.ac.uk>
2018-01-22 20:08:32 +0000

    Included additional options for distances between matrices

--- graphSimilarity/distance_functions.py
+++ graphSimilarity/distance_functions.py
@@ -1,4 +1,5 @@
 import numpy as np
+import scipy as sp
 from graph import Graph
 
 # All algorithms assume that the relevant graph matrices are already computed 
@@ -60,3 +61,46 @@ def matrix_ed(m1, m2):
 		dist += np.linalg.norm(m1[i] - m2[i])
 
 	return dist
+
+# --- Matrix Cosine Distance ---
+
+def matrix_cd(m1, m2):
+
+    dist = 0
+
+    for i in range(m1.shape[0]):
+
+        dist += sp.spatial.distance.cosine(m1[i], m2[i])
+
+    return dist
+
+# -- Matrix Manhattan Distance --
+
+def matrix_md(m1, m2):
+
+    dist = 0
+
+    for i in range(m1.shape[0]):
+
+        dist += sp.spatial.distance.cityblock(m1[i], m2[i])
+
+    return dist
+
+# --- Matrix Frobenius Distance ---
+
+def frob_dist(m1, m2):
+
+    dist = np.linalg.norm(m1 - m2, ord='fro')
+
+    return dist
+
+# --- Matrix Eigenvector Euclidean Distance ---
+
+def matrix_eig_ed(m1, m2):
+
+    w1, v1 = np.linalg.eig(m1)
+    w2, v2 = np.linalg.eig(m2)
+
+    dist = matrix_ed(v1, v2)
+
+    return dist
