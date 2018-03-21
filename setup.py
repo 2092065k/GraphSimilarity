4ffd1c9fb323e8376be1ed85e92e805011e4eaf1
2092065k <2092065k@student.gla.ac.uk>
2018-03-21 18:32:18 +0000

    Included a python setup.py file

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(name='graphSimilarity',
+      version='0.1',
+      description='Graph Distance Measures and Clustering',
+      author='Ivan Kyosev',
+      author_email='2092065k@student.gla.ac.uk',
+      license='GPL',
+      install_requires=['numpy',
+                        'matplotlib',
+                        'networkx',
+                        'scipy',
+                        'scikit_learn'
+                        ],
+      package_data={'graphSimilarity': ['README.md']},
+      packages=find_packages())
