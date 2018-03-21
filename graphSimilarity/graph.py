eec459cdf11c607431cfaf5e7594a58f9d72696a
2092065k <2092065k@student.gla.ac.uk>
2018-03-21 19:51:15 +0000

    Included documentation for the Graph class

--- graphSimilarity/graph.py
+++ graphSimilarity/graph.py
@@ -1,13 +1,14 @@
 import numpy as np
 
 class Graph:
-    'Simple class representing a graph'
+    'Class representing a graph with weighted edges and vertices'
 
     def __init__(self, num_vertices, edges, vertex_weights):
         self.num_vertices = num_vertices
         self.edges = edges
-        self.vertex_weights = np.ones(num_vertices)
 
+        # all vertices have a weight of one unless explicitly specified
+        self.vertex_weights = np.ones(num_vertices)
         for node_id in vertex_weights:
             self.vertex_weights[node_id] = vertex_weights[node_id]
 
@@ -20,7 +21,17 @@ class Graph:
     def get_vertex_weights(self):
         return self.vertex_weights
 
+    def get_adjacency_matrix(self):
+        return self.adjacency_matrix
+
+    def get_diagonal_matrix(self):
+        return self.diagonal_matrix
+
     def compute_adjacency_matrix(self, directed = False):
+        'Compute the adjacency matrix for the graph using the list of edges'
+
+        # if the edges are undirected, two locations in the
+        # adjacency matrix are modified for each edge in the list
         adjacency_matrix = np.zeros((self.num_vertices, self.num_vertices))
         for edge in self.edges:
             i, j, weight = edge
@@ -30,13 +41,11 @@ class Graph:
 
         self.adjacency_matrix = adjacency_matrix
 
-    def get_adjacency_matrix(self):
-        return self.adjacency_matrix
-
     def compute_diagonal_matrix(self):
+        'Compute the diaginal matrix for the graph using its adjacency matrix'
+
         diagonal_matrix = np.zeros(self.adjacency_matrix.shape)
         np.fill_diagonal(diagonal_matrix, self.adjacency_matrix.sum(axis=1))
         self.diagonal_matrix = diagonal_matrix
 
-    def get_diagonal_matrix(self):
-        return self.diagonal_matrix
+    
