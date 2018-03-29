d816fdd378fa7f029fad098b66c35f5e4e489a81
2092065k <2092065k@student.gla.ac.uk>
2018-03-29 21:27:44 +0100

    Correctly signify version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='graphSimilarity',
-      version='0.1',
+      version='0.1.0',
       description='Graph Distance Measures and Clustering',
       author='Ivan Kyosev',
       author_email='2092065k@student.gla.ac.uk',
